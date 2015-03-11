439b02563f07af2f207bb0701fab12616b09244d
Kyle Clemens <jkc.clemens@gmail.com>
2015-03-10 22:46:10 -0400

    Updated: Gist Markdown for help

--- src/main/java/org/royaldev/thehumanity/commands/impl/HelpCommand.java
+++ src/main/java/org/royaldev/thehumanity/commands/impl/HelpCommand.java
@@ -4,6 +4,7 @@ import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
+import org.json.JSONWriter;
 import org.pircbotx.User;
 import org.pircbotx.hooks.types.GenericMessageEvent;
 import org.royaldev.thehumanity.TheHumanity;
@@ -11,6 +12,7 @@ import org.royaldev.thehumanity.commands.CallInfo;
 import org.royaldev.thehumanity.commands.IRCCommand;
 import org.royaldev.thehumanity.commands.NoticeableCommand;
 
+import java.io.StringWriter;
 import java.util.Arrays;
 
 public class HelpCommand implements NoticeableCommand {
@@ -51,14 +53,27 @@ public class HelpCommand implements NoticeableCommand {
         final User u = event.getUser();
         final StringBuilder sb = new StringBuilder();
         for (final IRCCommand ic : this.humanity.getCommandHandler().getAll()) {
-            sb.append(this.humanity.getPrefix()).append(ic.getName()).append(" – ").append(ic.getDescription()).append("\n");
-            sb.append("  Usage: ").append(ic.getUsage().replaceAll("<command>", ic.getName())).append("\n");
-            if (ic.getAliases().length <= 0) continue;
-            sb.append("  Aliases: ").append(Arrays.toString(ic.getAliases())).append("\n");
+            sb.append("## ").append(this.humanity.getPrefix()).append(ic.getName()).append("\n");
+            sb.append("*").append(ic.getDescription()).append("*  \n");
+            if (!"<command>".equalsIgnoreCase(ic.getUsage())) {
+                sb.append("**Usage:** ").append(ic.getUsage().replaceAll("<command>", ic.getName())).append("  \n");
+            }
+            if (ic.getAliases().length > 0) {
+                sb.append("**Aliases:** ").append(Arrays.toString(ic.getAliases())).append("\n");
+            }
         }
+        final StringWriter sw = new StringWriter();
+        final JSONWriter jw = new JSONWriter(sw);
+        jw.object().key("files")
+            .object().key("help.md")
+            .object().key("content").value(sb.toString())
+            .endObject().endObject().endObject();
         try {
-            final HttpResponse<JsonNode> response = Unirest.post("http://hasteb.in/documents").body(sb.toString()).asJson();
-            this.notice(u, "http://hasteb.in/" + response.getBody().getObject().getString("key"));
+            final HttpResponse<JsonNode> response = Unirest
+                .post("https://api.github.com/gists")
+                .body(sw.toString())
+                .asJson();
+            this.notice(u, response.getBody().getObject().getString("html_url"));
         } catch (final UnirestException ex) {
             this.notice(u, "An error occurred: " + ex.getMessage());
         }
