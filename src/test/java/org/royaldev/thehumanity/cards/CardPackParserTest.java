c37ea79e88e424f926b0e003b663f0f272650a43
Kyle Clemens <jkc.clemens@gmail.com>
2015-03-30 02:51:04 -0400

    Added: Test for card pack name thing

---
+++ src/test/java/org/royaldev/thehumanity/cards/CardPackParserTest.java
@@ -0,0 +1,33 @@
+package org.royaldev.thehumanity.cards;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.hasItems;
+import static org.junit.Assert.assertThat;
+
+public class CardPackParserTest {
+
+    @Test
+    public void testGetListOfCardPackNames() throws Exception {
+        // Make up some pack names with expected command-line/argument syntax
+        final String[] packArray = new String[]{
+            "OneWord",
+            "\"With Spaces\"",
+            "'With Spaces, Round Two'",
+            "\"With \\\"Extra\\\" Quotes\"",
+            "'With \\'Extra\\' Quotes, Part Two'"
+        };
+        // Convert them
+        final List<String> packNames = CardPackParser.getListOfCardPackNames(packArray);
+        // Make sure they turn out correctly
+        assertThat(packNames, hasItems(
+            "OneWord",
+            "With Spaces",
+            "With Spaces, Round Two",
+            "With \"Extra\" Quotes",
+            "With 'Extra' Quotes, Part Two"
+        ));
+    }
+}
