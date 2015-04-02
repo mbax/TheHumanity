b3d078d4b1781071de9fe825ef59ead06798981a
Kyle Clemens <jkc.clemens@gmail.com>
2015-04-01 20:11:32 -0400

    Fixed: Remove empty split strings

--- src/main/java/org/royaldev/thehumanity/BaseListeners.java
+++ src/main/java/org/royaldev/thehumanity/BaseListeners.java
@@ -1,5 +1,6 @@
 package org.royaldev.thehumanity;
 
+import com.google.common.base.Splitter;
 import org.kitteh.irc.client.library.event.channel.ChannelInviteEvent;
 import org.kitteh.irc.client.library.event.channel.ChannelJoinEvent;
 import org.kitteh.irc.client.library.event.channel.ChannelKickEvent;
@@ -13,7 +14,7 @@ import org.royaldev.thehumanity.commands.CallInfo;
 import org.royaldev.thehumanity.commands.IRCCommand;
 import org.royaldev.thehumanity.util.ConversionHelper;
 
-import java.util.Arrays;
+import java.util.List;
 
 /**
  * The basic listeners of the bot that allow it to function.
@@ -47,15 +48,16 @@ final class BaseListeners {
     public void onChannelMessage(final ChannelMessageEvent e) {
         final String message = e.getMessage();
         if (message.isEmpty() || message.charAt(0) != this.humanity.getPrefix()) return;
-        final String[] split = message.trim().split(" ");
-        final String commandString = split[0].substring(1, split[0].length());
+        final List<String> split = Splitter.on(' ').omitEmptyStrings().splitToList(message.trim());
+        final String commandString = split.get(0).substring(1, split.get(0).length());
         final IRCCommand command = this.humanity.getCommandHandler().get(commandString);
         if (command == null) return;
         final IRCCommand.CommandType commandType = command.getCommandType();
         if (commandType != IRCCommand.CommandType.MESSAGE && commandType != IRCCommand.CommandType.BOTH) return;
         this.humanity.getLogger().info(e.getChannel().getName() + "/" + e.getActor().getNick() + ": " + e.getMessage());
         try {
-            command.onCommand(e, new CallInfo(commandString, CallInfo.UsageType.MESSAGE), Arrays.copyOfRange(split, 1, split.length));
+            final List<String> args = split.subList(1, split.size());
+            command.onCommand(e, new CallInfo(commandString, UsageType.MESSAGE), args.toArray(new String[args.size()]));
         } catch (final Throwable t) {
             t.printStackTrace();
             final StringBuilder sb = new StringBuilder("Unhandled command exception! ");
@@ -99,8 +101,8 @@ final class BaseListeners {
     public void onPrivateMessage(final PrivateMessageEvent e) {
         final String message = e.getMessage();
         if (message.isEmpty()) return;
-        final String[] split = message.trim().split(" ");
-        final String commandString = split[0];
+        final List<String> split = Splitter.on(' ').omitEmptyStrings().splitToList(message.trim());
+        final String commandString = split.get(0);
         final IRCCommand command = this.humanity.getCommandHandler().get(commandString);
         if (command == null) {
             ConversionHelper.respond(e, "No such command.");
@@ -113,7 +115,8 @@ final class BaseListeners {
         }
         this.humanity.getLogger().info(e.getActor().getNick() + ": " + e.getMessage());
         try {
-            command.onCommand(e, new CallInfo(commandString, CallInfo.UsageType.PRIVATE), Arrays.copyOfRange(split, 1, split.length));
+            final List<String> args = split.subList(1, split.size());
+            command.onCommand(e, new CallInfo(commandString, CallInfo.UsageType.PRIVATE), args.toArray(new String[args.size()]));
         } catch (final Throwable t) {
             t.printStackTrace();
             final StringBuilder sb = new StringBuilder("Unhandled command exception! ");
