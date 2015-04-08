6bbe6e78c82d2edc43d39114f1714fe8d0ac038b
Kyle Clemens <jkc.clemens@gmail.com>
2015-04-08 00:32:59 -0400

    Fixed: I swear

--- src/main/java/org/royaldev/thehumanity/Game.java
+++ src/main/java/org/royaldev/thehumanity/Game.java
@@ -150,7 +150,8 @@ public class Game {
     @NotNull
     public String antiPing(@NotNull String message) {
         Preconditions.checkNotNull(message, "message was null");
-        for (final String nickname : this.channel.getNicknames()) {
+        for (final User user : this.channel.getUsers()) { // TODO: Use nicknames when it's not stupid (non-lowercase)
+            final String nickname = user.getNick();
             if (nickname.length() <= 1) continue;
             message = message.replace(nickname, nickname.substring(0, 1) + "\u200b" + nickname.substring(1));
         }
