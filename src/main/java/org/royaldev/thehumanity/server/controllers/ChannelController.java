e1539eba501c8ac2feabee278dcd62253afec886
Kyle Clemens <jkc.clemens@gmail.com>
2015-04-11 13:48:30 -0400

    Added: Channel API

--- src/main/java/org/royaldev/thehumanity/server/controllers/ChannelController.java
+++ src/main/java/org/royaldev/thehumanity/server/controllers/ChannelController.java
@@ -1,6 +1,7 @@
 package org.royaldev.thehumanity.server.controllers;
 
 import org.kitteh.irc.client.library.element.Channel;
+import org.kitteh.irc.client.library.element.User;
 import org.royaldev.thehumanity.TheHumanity;
 import org.royaldev.thehumanity.server.services.channel.ChannelService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.stream.Collectors;
 
 @Controller
 public class ChannelController {
@@ -17,6 +22,40 @@ public class ChannelController {
     private ChannelService channelService;
     @Autowired
     private TheHumanity humanity;
+
+    @ResponseBody
+    @RequestMapping(value = "/api/channels", method = RequestMethod.GET, produces = APIHelper.PRODUCES)
+    public String apiViewAllChannels(final HttpServletResponse response) {
+        return APIHelper.makeObjectMapperJSON(
+            response,
+            om -> om.writeValueAsString(this.channelService.getAll().stream().map(Channel::getName).collect(Collectors.toSet()))
+        );
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/api/channel/{channel}", method = RequestMethod.GET, produces = APIHelper.PRODUCES)
+    public String apiViewChannel(@PathVariable final String channel, final HttpServletResponse response) {
+        final Channel c = this.channelService.getFromName("#" + channel);
+        if (c == null || !this.humanity.getBot().getChannels().contains(c)) {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            return APIHelper.makeError("No such channel.");
+        }
+        return APIHelper.makeJSON(jw -> {
+            jw.array();
+            for (final User user : c.getUsers()) {
+                jw
+                    .object()
+                    .key("nickname")
+                    .value(user.getNick())
+                    .key("user")
+                    .value(user.getUser())
+                    .key("hostname")
+                    .value(user.getHost())
+                    .endObject();
+            }
+            jw.endArray();
+        });
+    }
 
     @RequestMapping(value = "/channels", method = RequestMethod.GET)
     public String viewAllChannels(final Model model) {
