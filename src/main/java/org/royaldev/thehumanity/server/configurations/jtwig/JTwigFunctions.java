c3a6d0b3ee4733e009e64c14970d3662e490e8c0
Kyle Clemens <jkc.clemens@gmail.com>
2015-04-17 16:51:28 -0400

    Added: Topic

--- src/main/java/org/royaldev/thehumanity/server/configurations/jtwig/JTwigFunctions.java
+++ src/main/java/org/royaldev/thehumanity/server/configurations/jtwig/JTwigFunctions.java
@@ -8,32 +8,37 @@ import org.kitteh.irc.client.library.IRCFormat;
 public class JTwigFunctions {
 
     @JtwigFunction(name = "br2nl")
-    public String br2nl(@Parameter String input) {
+    public String br2nl(@Parameter final String input) {
         return input.replaceAll("<br/?>", "\n");
     }
 
     @JtwigFunction(name = "irc_bold")
-    public String emboldenIRC(@Parameter String input) {
+    public String emboldenIRC(@Parameter final String input) {
         return input.replace(IRCFormat.BOLD.toString(), "<strong>").replace(IRCFormat.RESET.toString(), "</strong>");
     }
 
     @JtwigFunction(name = "friendly_enum")
-    public String friendlyEnum(@Parameter String s) {
+    public String friendlyEnum(@Parameter final String s) {
         return StringUtils.capitalize(s.replace('_', ' ').toLowerCase());
     }
 
     @JtwigFunction(name = "plural")
-    public String pluralize(@Parameter String input, @Parameter int number) {
+    public String pluralize(@Parameter final String input, @Parameter final int number) {
         return this.pluralize(input, number, "s");
     }
 
     @JtwigFunction(name = "plural")
-    public String pluralize(@Parameter String input, @Parameter int number, @Parameter String suffix) {
+    public String pluralize(@Parameter final String input, @Parameter final int number, @Parameter final String suffix) {
         return number == 1 ? input : input + suffix;
     }
 
+    @JtwigFunction(name = "remove_irc_colors")
+    public String removeIRCColors(@Parameter final String input) {
+        return IRCFormat.stripColor(input);
+    }
+
     @JtwigFunction(name = "remove_irc_formatting")
-    public String removeIRCFormatting(@Parameter String input) {
+    public String removeIRCFormatting(@Parameter final String input) {
         return IRCFormat.stripFormating(input);
     }
 
